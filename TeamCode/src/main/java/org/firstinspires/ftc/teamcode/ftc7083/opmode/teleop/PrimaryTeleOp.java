3c06cf2a244151a5b1a982bca22515145ccf6dba
Roy Brabson <rbrabson@gmail.com>
2024-10-07 16:42:32 -0400

    merge Bot-A

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/opmode/teleop/PrimaryTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/opmode/teleop/PrimaryTeleOp.java
@@ -8,9 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.ftc7083.Robot;
-import org.firstinspires.ftc.teamcode.ftc7083.subsystem.ScoringSubsystem;
 import org.firstinspires.ftc.teamcode.ftc7083.subsystem.controller.MecanumDriveController;
-import org.firstinspires.ftc.teamcode.ftc7083.subsystem.controller.ScoringSubsystemController;
 import org.firstinspires.ftc.teamcode.ftc7083.subsystem.controller.SubsystemController;
 
 import java.util.Arrays;
@@ -31,9 +29,7 @@ public class PrimaryTeleOp extends OpMode {
         robot = Robot.init(hardwareMap, telemetry);
 
         MecanumDriveController mecanumDriveController = new MecanumDriveController(robot.mecanumDrive, telemetry);
-        ScoringSubsystem  scoringSubsystem= new ScoringSubsystem(robot.arm, robot.lift, telemetry);
-        ScoringSubsystemController scoringSubsystemController = new ScoringSubsystemController(scoringSubsystem);
-        controllers = Arrays.asList(mecanumDriveController, scoringSubsystemController);
+        controllers = Arrays.asList(mecanumDriveController);
 
         telemetry.addLine("Initialization Complete");
         telemetry.update();
